941bc0af121997cc2235efee758248b8447ed851
Mohamad Rizal Andy Alkarim <rzalandy@gmail.com>
2021-04-15 11:58:20 +0700

    feat(template): update detail porto

--- application/views/Detail_portofolio.php
+++ application/views/Detail_portofolio.php
@@ -12,13 +12,13 @@
                     <div class="qodef-container-inner">
                         <div class="qodef-title-subtitle-holder" style="">
                             <div class="qodef-title-subtitle-holder-inner">
-                                <h1><span>Wedding Bells</span></h1>
+                                <h1><span>Andy & Trie</span></h1>
                                 <div class="qodef-breadcrumbs-holder">
                                     <div class="qodef-breadcrumbs">
                                         <div class="qodef-breadcrumbs-inner"><a
-                                                href="https://moments.qodeinteractive.com/">Moments</a><span
+                                                href="<?php echo base_url('portofolio') ?>">Portofolio</a><span
                                                 class="qodef-delimiter">&nbsp;/&nbsp;</span><span
-                                                class="qodef-current">Wedding Bells</span></div>
+                                                class="qodef-current">Andy & Trie</span></div>
                                     </div>
                                 </div>
                             </div>
@@ -35,30 +35,24 @@
                             <div class="qodef-column-inner">
                                 <div class="qodef-portfolio-media qodef-owl-slider">
                                     <div class="qodef-portfolio-single-media">
-                                        <a itemprop="image" title="portfolio-gallery-image-1"
-                                            data-rel="prettyPhoto[single_pretty_photo]"
-                                            href="https://moments.qodeinteractive.com/wp-content/uploads/2016/06/portfolio-gallery-image-1.jpg">
+                                        <a itemprop="image" title="" data-rel="prettyPhoto[single_pretty_photo]"
+                                            href="<?php echo base_url() . 'assets/'; ?>images/dp1.jpg">
                                             <img itemprop="image"
-                                                src="https://moments.qodeinteractive.com/wp-content/uploads/2016/06/portfolio-gallery-image-1.jpg"
-                                                alt="a" />
+                                                src="<?php echo base_url() . 'assets/'; ?>images/dp1.jpg" alt="a" />
                                         </a>
                                     </div>
                                     <div class="qodef-portfolio-single-media">
-                                        <a itemprop="image" title="portfolio-gallery-image-2"
-                                            data-rel="prettyPhoto[single_pretty_photo]"
-                                            href="https://moments.qodeinteractive.com/wp-content/uploads/2016/06/portfolio-gallery-image-2.jpg">
+                                        <a itemprop="image" title="" data-rel="prettyPhoto[single_pretty_photo]"
+                                            href="<?php echo base_url() . 'assets/'; ?>images/dp2.jpg">
                                             <img itemprop="image"
-                                                src="https://moments.qodeinteractive.com/wp-content/uploads/2016/06/portfolio-gallery-image-2.jpg"
-                                                alt="a" />
+                                                src="<?php echo base_url() . 'assets/'; ?>images/dp2.jpg" alt="a" />
                                         </a>
                                     </div>
                                     <div class="qodef-portfolio-single-media">
-                                        <a itemprop="image" title="portfolio-gallery-image-3"
-                                            data-rel="prettyPhoto[single_pretty_photo]"
-                                            href="https://moments.qodeinteractive.com/wp-content/uploads/2016/06/portfolio-gallery-image-3.jpg">
+                                        <a itemprop="image" title="" data-rel="prettyPhoto[single_pretty_photo]"
+                                            href="<?php echo base_url() . 'assets/'; ?>images/dp3.jpg">
                                             <img itemprop="image"
-                                                src="https://moments.qodeinteractive.com/wp-content/uploads/2016/06/portfolio-gallery-image-3.jpg"
-                                                alt="a" />
+                                                src="<?php echo base_url() . 'assets/'; ?>images/dp3.jpg" alt="a" />
                                         </a>
                                     </div>
                                 </div>
@@ -68,7 +62,7 @@
                             <div class="qodef-column-inner">
                                 <div class="qodef-portfolio-info-holder">
                                     <div class="qodef-portfolio-info-item">
-                                        <h3>Wedding Bells</h3>
+                                        <h3>Andy & Trie</h3>
                                         <div class="qodef-portfolio-content">
                                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
                                                 sollicitudin, tellus vitae condimentum egestas, libero dolor
@@ -81,56 +75,17 @@
                                         </div>
                                     </div>
                                     <div class="qodef-portfolio-info-item qodef-portfolio-date">
-                                        <h6>Date</h6>
+                                        <h6>Tanggal</h6>
                                         <p>June 9, 2016</p>
                                     </div>
                                     <div class="qodef-portfolio-info-item qodef-portfolio-categories">
-                                        <h6>Category</h6>
+                                        <h6>Vendor</h6>
 
                                         <p>
-                                            Wedding </p>
-                                    </div>
-                                    <div class="qodef-portfolio-social">
-                                        <div class="qodef-social-share-holder qodef-list">
-                                            <ul>
-                                                <li class="qodef-facebook-share">
-                                                    <a itemprop="url" class="qodef-share-link" href="#"
-                                                        onclick="window.open('http://m.facebook.com/sharer.php?u=https%3A%2F%2Fmoments.qodeinteractive.com%2Fportfolio-item%2Fwedding-bells%2F');">
-                                                        <span class="qodef-social-network-icon social_facebook"></span>
-                                                    </a>
-                                                </li>
-                                                <li class="qodef-twitter-share">
-                                                    <a itemprop="url" class="qodef-share-link" href="#"
-                                                        onclick="window.open('https://twitter.com/intent/tweet?text=Lorem+ipsum+dolor+sit+amet%2C+consectetur+adipiscing+elit.+Cras+sollicitudin%2C+tellus+vitae+condimentum+egestas%2C+libero+https://moments.qodeinteractive.com/portfolio-item/wedding-bells/', 'popupwindow', 'scrollbars=yes,width=800,height=400');">
-                                                        <span class="qodef-social-network-icon social_twitter"></span>
-                                                    </a>
-                                                </li>
-                                                <li class="qodef-pinterest-share">
-                                                    <a itemprop="url" class="qodef-share-link" href="#"
-                                                        onclick="popUp=window.open('http://pinterest.com/pin/create/button/?url=https%3A%2F%2Fmoments.qodeinteractive.com%2Fportfolio-item%2Fwedding-bells%2F&amp;description=Wedding Bells&amp;media=https%3A%2F%2Fmoments.qodeinteractive.com%2Fwp-content%2Fuploads%2F2016%2F06%2Fportfolio-pinterest-image-2.jpg', 'popupwindow', 'scrollbars=yes,width=800,height=400');popUp.focus();return false;">
-                                                        <span class="qodef-social-network-icon social_pinterest"></span>
-                                                    </a>
-                                                </li>
-                                            </ul>
-                                        </div>
+                                            Vendor A, Vendor B, Vendor C </p>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    <div class="qodef-portfolio-single-nav">
-                        <div class="qodef-portfolio-prev">
-                            <a href="https://moments.qodeinteractive.com/portfolio-item/brides-bouquet/"
-                                rel="prev"><span class="fa fa-angle-left"></span></a>
-                        </div>
-                        <div class="qodef-portfolio-back-btn">
-                            <a itemprop="url" href="https://moments.qodeinteractive.com/about-us/">
-                                <span class="fa fa-th"></span>
-                            </a>
-                        </div>
-                        <div class="qodef-portfolio-next">
-                            <a href="https://moments.qodeinteractive.com/portfolio-item/happy-couple/" rel="next"><span
-                                    class="fa fa-angle-right"></span></a>
                         </div>
                     </div>
                 </div>
