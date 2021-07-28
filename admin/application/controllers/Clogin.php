0539c9fa1f585f3da9a157d283c3ba245b5d46c3
Mohamad Rizal Andy Alkarim <rzalandy@gmail.com>
2021-07-28 08:44:23 +0700

    feat(update): clogin

--- admin/application/controllers/Clogin.php
+++ admin/application/controllers/Clogin.php
@@ -1,49 +1,48 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Clogin extends CI_Controller {
+class Clogin extends CI_Controller
+{
 
-	public function __construct()
-	{
-		parent::__construct();
-		$this->load->library('form_validation');
-		$this->load->model('mlogin');
-	}
-
-	private function logged_in()
+    public function __construct()
     {
-        if( ! $this->session->userdata('logged_in')){
-            redirect('clogin');
-        }
+        parent::__construct();
+        $this->load->library('form_validation');
+        $this->load->model('mlogin');
     }
 
-	public function index()
-	{
-		$this->load->view('index');
-	}
+    // private function logged_in()
+    // {
+    //     if( ! $this->session->userdata('logged_in')){
+    //         redirect('clogin');
+    //     }
+    // }
 
-	function loginproses()
+    public function index()
     {
-		$username = $this->input->post('username', TRUE);
-		$password = $this->input->post('pass', TRUE);
-		$validate = $this->mlogin->validate($username,$password);
-
-		if($validate->num_rows() > 0){
-        $data  = $validate->row_array();
-        $nama = $data['username'];
-
-        $sesdata = array(
-            'username'  => $nama,
-            'logged_in' => TRUE
-        );
-
-        $this->session->set_userdata($sesdata);
-
-        redirect('panel');
+        $this->load->view('index');
     }
-    else
-        {
-            echo $this->session->set_flashdata('msg','Username or Password is Wrong');
+
+    function loginproses()
+    {
+        $username = $this->input->post('username', TRUE);
+        $password = $this->input->post('pass', TRUE);
+        $validate = $this->mlogin->validate($username, $password);
+
+        if ($validate->num_rows() > 0) {
+            $data  = $validate->row_array();
+            $nama = $data['username'];
+
+            $sesdata = array(
+                'username'  => $nama,
+                'logged_in' => TRUE
+            );
+
+            $this->session->set_userdata($sesdata);
+
+            redirect('panel');
+        } else {
+            echo $this->session->set_flashdata('msg', 'Username or Password is Wrong');
             redirect('clogin');
         }
     }
